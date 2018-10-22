53594ec97c287ec038bac35e6f42268b0cf9ebea
Estevan Maito <ejmaito@gmail.com>
2018-10-22 14:23:56 -0300

    Updates

---
+++ precache-manifest.e11267e92737d5925a3e2eb0ee925cde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3e1419df2a1ac00aadeb8d1b62fe4c7",
+    "url": "/react-pomodoro/static/media/bell.f3e1419d.mp3"
+  },
+  {
+    "revision": "3d44c52309990baf2f61",
+    "url": "/react-pomodoro/static/js/runtime~main.3d44c523.js"
+  },
+  {
+    "revision": "cb710d424394a2a5400d",
+    "url": "/react-pomodoro/static/js/main.cb710d42.chunk.js"
+  },
+  {
+    "revision": "458474106fd2655c30ae",
+    "url": "/react-pomodoro/static/js/1.45847410.chunk.js"
+  },
+  {
+    "revision": "cb710d424394a2a5400d",
+    "url": "/react-pomodoro/static/css/main.5825d0d4.chunk.css"
+  },
+  {
+    "revision": "fd0fc575af935ccf6179dac202af9e57",
+    "url": "/react-pomodoro/index.html"
+  }
+];
